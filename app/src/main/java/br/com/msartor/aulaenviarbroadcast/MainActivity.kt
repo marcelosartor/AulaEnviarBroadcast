ee54f3d42ce8c8952448fdb1cf80489bf536bd54
marcelosartor <marcelo_sartor@hotmail.com>
2025-04-21 11:48:51 -0300

    feat: Open File

--- app/src/main/java/br/com/msartor/aulaenviarbroadcast/MainActivity.kt
+++ app/src/main/java/br/com/msartor/aulaenviarbroadcast/MainActivity.kt
@@ -1,6 +1,8 @@
 package br.com.msartor.aulaenviarbroadcast
 
+import android.content.ComponentName
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
@@ -26,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnAbrirPdf.setOnClickListener {
-            Log.i("ComunicacaoBroadcastReceiver", "ComunicacaoBroadcastReceiver clicou no botao ")
+            Log.i("ComunicacaoBroadcastReceiver", "ComunicacaoBroadcastReceiver clicou no botao abrir pdf")
             val intent = Intent("br.com.msartor.aulaservice.ABRIR_ARQUIVO_PDF")
             val bundle = Bundle()
             bundle.putString("ARQUIVO","arquivo/perfil.pdf")
@@ -34,5 +36,47 @@ class MainActivity : AppCompatActivity() {
 
             sendBroadcast(intent)
         }
+
+        binding.btnAbrirPdfExplicita.setOnClickListener {
+            Log.i("ComunicacaoBroadcastReceiver", "ComunicacaoBroadcastReceiver clicou no botao abrir pdf explicita ")
+            val intent = Intent("br.com.msartor.aulaservice.ABRIR_ARQUIVO_PDF")
+            val bundle = Bundle()
+            bundle.putString("ARQUIVO","arquivo/perfil.pdf")
+            intent.putExtras(bundle)
+
+            val component = ComponentName("br.com.msartor.aulaservice", "br.com.msartor.aulaservice.ComunicacaoBroadcastActivity")
+            intent.component = component
+            sendBroadcast(intent)
+
+        }
+
+        binding.btnIntentImplicita.setOnClickListener {
+            Log.i(
+                "ComunicacaoBroadcastReceiver",
+                "ComunicacaoBroadcastReceiver clicou no botao Intent Implicita"
+            )
+            val link = "https://www.google.com"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+            startActivity(intent)
+
+
+        }
+
+        binding.btnIntentImplicitaWithChooser.setOnClickListener{
+            Log.i(
+                "ComunicacaoBroadcastReceiver",
+                "ComunicacaoBroadcastReceiver clicou no botao Intent Implicita com Escolha"
+            )
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, "Texto a ser enviado")
+            intent.type = "text/plain"
+
+            val compartilhar = Intent.createChooser(intent, "Compartilhar")
+            startActivity(compartilhar)
+
+        }
+
+
     }
 }
